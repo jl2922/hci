c9c0436ff97940c13870f6ce9673be4e1c1214b4
Junhao Li <jl2922@cornell.edu>
2017-09-29 14:34:55 -0400

    .

---
+++ src/heg/heg_system.h
@@ -0,0 +1,9 @@
+#ifndef HEG_SYSTEM_H_
+#define HEG_SYSTEM_H_
+
+class HEGSystem {
+ public:
+  virtual ~HEGSystem() = default;
+};
+
+#endif
---
+++ src/solver/solver.h
@@ -0,0 +1,9 @@
+#ifndef SOLVER_H_
+#define SOLVER_H_
+
+class Solver {
+ public:
+  virtual ~Solver() = default;
+};
+
+#endif
