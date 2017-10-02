f4be1227ef5305e583780caae181760146e4bf78
Junhao Li <jl2922@cornell.edu>
2017-10-02 14:17:13 -0400

    variation interface (#5)

---
+++ src/heg/k_points_util.h
@@ -0,0 +1,17 @@
+#ifndef K_POINTS_UTIL_H_
+#define K_POINTS_UTIL_H_
+
+#include <array>
+#include <vector>
+
+class KPointsUtil {
+ public:
+  static int get_n_k_points(const double);
+
+  static std::vector<std::array<int8_t, 3>> generate_k_points(const double);
+
+  static std::vector<std::array<int8_t, 3>> get_k_points_diffs(
+      const std::vector<std::array<int8_t, 3>>&);
+};
+
+#endif
