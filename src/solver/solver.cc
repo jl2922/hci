995c537fe51ca744bf14294b4d6cfa4413da2a92
Junhao Li <jl2922@cornell.edu>
2017-10-03 15:02:50 -0400

    find connected var dets (#7)

--- src/solver/solver.cc
+++ src/solver/solver.cc
@@ -1,6 +1,12 @@
 #include "solver.h"
 
+#include <cmath>
+#include <string>
+#include <unordered_set>
+#include <utility>
 #include "../injector.h"
+
+#define ENERGY_FORMAT "%.12f"
 
 class SolverImpl : public Solver {
  public:
@@ -16,6 +22,18 @@ class SolverImpl : public Solver {
   void perturbation(const double eps_pt) override;
 
  private:
+  int n_up = 0;
+
+  int n_dn = 0;
+
+  double energy_hf = 0.0;
+
+  double energy_var = 0.0;
+
+  double energy_pt = 0.0;
+
+  bool verbose = false;
+
   Session* const session;
 
   const std::unique_ptr<Connections> connections;
@@ -26,14 +44,104 @@ class SolverImpl : public Solver {
 SolverImpl::SolverImpl(
     Session* const session,
     Connections* const connections,
-    AbstractSystem* const abstrct_system)
+    AbstractSystem* const abstract_system)
     : session(session),
       connections(connections),
-      abstract_system(abstract_system) {}
+      abstract_system(abstract_system) {
+  verbose = session->get_parallel()->is_master();
+  Config* const config = session->get_config();
+  n_up = config->get_int("n_up");
+  n_dn = config->get_int("n_dn");
+}
 
-void SolverImpl::setup_hf(){};
+void SolverImpl::setup_hf() {
+  // Create or clear wavefunction.
+  if (!abstract_system->wf) {
+    abstract_system->wf.reset(new data::Wavefunction());
+  }
+  data::Wavefunction* const wf = abstract_system->wf.get();
+  wf->Clear();
 
-void SolverImpl::variation(const double eps_var){};
+  // Add a single term with coef 1.0 and no diffs.
+  data::Term* term = wf->add_terms();
+  term->set_coef(1.0);
+  data::Determinant* det_hf = term->mutable_det();
+  data::SpinDeterminant* up = det_hf->mutable_up();
+  up->set_n_hf_elecs(n_up);
+  data::SpinDeterminant* dn = det_hf->mutable_dn();
+  dn->set_n_hf_elecs(n_dn);
+
+  // Update HF and variational energy.
+  energy_hf = energy_var = abstract_system->hamiltonian(det_hf, det_hf);
+  if (verbose) printf("HF det setup.\n");
+};
+
+void SolverImpl::variation(const double eps_var) {
+  Timer* const timer = session->get_timer();
+
+  // Contruct variational determinant hash set.
+  data::Wavefunction* const wf = abstract_system->wf.get();
+  const int n_start_dets = wf->terms_size();
+  std::unordered_set<std::string> var_dets_set;
+  std::vector<double> prev_coefs;
+  var_dets_set.reserve(n_start_dets);
+  prev_coefs.resize(n_start_dets, 0.0);
+  for (const auto& term : wf->terms()) {
+    std::string det_code;
+    term.det().SerializeToString(&det_code);
+    var_dets_set.insert(std::move(det_code));
+  }
+
+  // Variation iterations.
+  bool converged = false;
+  int iteration = 0;
+  int n_new_dets = 0;
+  double energy_var_new = 0.0;
+  const auto& connected_det_handler =
+      [&](const data::Determinant* const connected_det) {
+        std::string det_code;
+        connected_det->SerializeToString(&det_code);
+        if (var_dets_set.count(det_code) == 0) {
+          var_dets_set.insert(std::move(det_code));
+          data::Term* const term = wf->add_terms();
+          term->set_coef(0.0);
+          auto connected_det_copy = new data::Determinant(*connected_det);
+          term->set_allocated_det(connected_det_copy);
+          n_new_dets++;
+        }
+      };
+  while (!converged) {
+    timer->start("Variation: " + std::to_string(iteration));
+    n_new_dets = 0;
+    const int n_old_dets = wf->terms_size();
+    for (int i = 0; i < n_old_dets; i++) {
+      const auto& term = wf->terms(i);
+      const double coef = term.coef();
+      if (std::abs(coef) <= std::abs(prev_coefs[i])) continue;
+      abstract_system->find_connected_dets(
+          &term.det(), eps_var / std::abs(term.coef()), connected_det_handler);
+    }
+    const int n_total_dets = wf->terms_size();
+    if (verbose) {
+      printf("New / total dets: %'d / %'d\n", n_new_dets, n_total_dets);
+    }
+    timer->checkpoint("found new dets");
+
+    // Update prev coefs.
+    prev_coefs.resize(n_total_dets);
+    for (int i = 0; i < n_total_dets; i++) prev_coefs[i] = wf->terms(i).coef();
+
+    // Diagonalization.
+
+    timer->end();
+
+    if (std::abs(energy_var_new - energy_var) < 1.0e-6) {
+      converged = true;
+    }
+    energy_var = energy_var_new;
+    iteration++;
+  }
+};
 
 void SolverImpl::perturbation(const double eps_pt){};
 
---
+++ src/solver/spin_det_util.cc
@@ -0,0 +1,178 @@
+#include "spin_det_util.h"
+
+// Converting from HF diffs representation to occupied orbitals.
+std::vector<int> SpinDetUtil::get_occupied_orbitals(
+    const data::SpinDeterminant& spin_det) {
+  std::vector<int> occ;
+  const int n_hf_elecs = spin_det.n_hf_elecs();
+  occ.reserve(n_hf_elecs);
+  int level = 0;
+  for (const int v_orb : spin_det.v_holes()) {
+    while (level < v_orb) {
+      occ.push_back(level);
+      level++;
+    }
+    level = v_orb + 1;
+  }
+  while (level < n_hf_elecs) {
+    occ.push_back(level);
+    level++;
+  }
+  for (const int c_orb : spin_det.c_elecs()) {
+    occ.push_back(c_orb);
+  }
+  return occ;
+};
+
+bool SpinDetUtil::is_occupied(
+    const data::SpinDeterminant& spin_det, const int orb) {
+  const int n_hf_elecs = spin_det.n_hf_elecs();
+  if (orb < n_hf_elecs) {
+    for (const int v_orb : spin_det.v_holes()) {
+      if (v_orb == orb) return false;
+      if (v_orb > orb) return true;
+    }
+    return true;
+  } else {
+    for (const int c_orb : spin_det.c_elecs()) {
+      if (c_orb == orb) return true;
+      if (c_orb > orb) return false;
+    }
+    return false;
+  }
+}
+
+int SpinDetUtil::get_n_lower_elecs(
+    const data::SpinDeterminant& spin_det, const int orb) {
+  const int n_hf_elecs = spin_det.n_hf_elecs();
+  if (orb < n_hf_elecs) {
+    int res = orb;
+    for (const int v_orb : spin_det.v_holes()) {
+      if (v_orb == orb) return 0;
+      if (v_orb > orb) return res;
+      res--;
+    }
+    return res;
+  } else {
+    int res = n_hf_elecs - spin_det.v_holes_size();
+    for (const int c_orb : spin_det.c_elecs()) {
+      if (c_orb == orb) return res;
+      if (c_orb > orb) return 0;
+      res++;
+    }
+    return 0;
+  }
+}
+
+void SpinDetUtil::set_occupation(
+    data::SpinDeterminant* spin_det, const int orb, const bool occ) {
+  const int n_hf_elecs = spin_det->n_hf_elecs();
+  if (orb < n_hf_elecs) {
+    if (occ) {
+      remove_orbital(spin_det->mutable_v_holes(), orb);
+    } else {
+      insert_orbital(spin_det->mutable_v_holes(), orb);
+    }
+  } else {
+    if (occ) {
+      insert_orbital(spin_det->mutable_c_elecs(), orb);
+    } else {
+      remove_orbital(spin_det->mutable_c_elecs(), orb);
+    }
+  }
+}
+
+std::vector<int> SpinDetUtil::get_eor(
+    const data::SpinDeterminant& lhs, const data::SpinDeterminant& rhs) {
+  std::vector<int> eor_orbs;
+  eor_orbs.reserve(5);
+
+  // Process v holes.
+  int lhs_ptr = 0;
+  int rhs_ptr = 0;
+  int lhs_size = lhs.v_holes_size();
+  int rhs_size = rhs.v_holes_size();
+  while (lhs_ptr < lhs_size && rhs_ptr < rhs_size) {
+    if (lhs.v_holes(lhs_ptr) == rhs.v_holes(rhs_ptr)) {
+      lhs_ptr++;
+      rhs_ptr++;
+    } else if (lhs.v_holes(lhs_ptr) > rhs.v_holes(rhs_ptr)) {
+      eor_orbs.push_back(rhs.v_holes(rhs_ptr));
+      rhs_ptr++;
+    } else {
+      eor_orbs.push_back(lhs.v_holes(lhs_ptr));
+      lhs_ptr++;
+    }
+    if (eor_orbs.size() > 4) return eor_orbs;
+  }
+  while (lhs_ptr < lhs_size) {
+    eor_orbs.push_back(lhs.v_holes(lhs_ptr));
+    if (eor_orbs.size() > 4) return eor_orbs;
+    lhs_ptr++;
+  }
+  while (rhs_ptr < rhs_size) {
+    eor_orbs.push_back(rhs.v_holes(rhs_ptr));
+    if (eor_orbs.size() > 4) return eor_orbs;
+    rhs_ptr++;
+  }
+
+  // Process c elecs.
+  lhs_ptr = 0;
+  rhs_ptr = 0;
+  lhs_size = lhs.c_elecs_size();
+  rhs_size = rhs.c_elecs_size();
+  while (lhs_ptr < lhs_size && rhs_ptr < rhs_size) {
+    if (lhs.c_elecs(lhs_ptr) == rhs.c_elecs(rhs_ptr)) {
+      lhs_ptr++;
+      rhs_ptr++;
+    } else if (lhs.c_elecs(lhs_ptr) > rhs.c_elecs(rhs_ptr)) {
+      eor_orbs.push_back(rhs.c_elecs(rhs_ptr));
+      rhs_ptr++;
+    } else {
+      eor_orbs.push_back(lhs.c_elecs(lhs_ptr));
+      lhs_ptr++;
+    }
+    if (eor_orbs.size() > 4) return eor_orbs;
+  }
+  while (lhs_ptr < lhs_size) {
+    eor_orbs.push_back(lhs.c_elecs(lhs_ptr));
+    if (eor_orbs.size() > 4) return eor_orbs;
+    lhs_ptr++;
+  }
+  while (rhs_ptr < rhs_size) {
+    eor_orbs.push_back(rhs.c_elecs(rhs_ptr));
+    if (eor_orbs.size() > 4) return eor_orbs;
+    rhs_ptr++;
+  }
+
+  return eor_orbs;
+}
+
+void SpinDetUtil::insert_orbital(
+    google::protobuf::RepeatedField<google::protobuf::int32>* t_orbs,
+    const int orb) {
+  t_orbs->Add();
+  const int n_t_orbs = t_orbs->size();
+  int i;
+  for (i = n_t_orbs - 1; i >= 1; i--) {
+    const int t_orb = t_orbs->Get(i - 1);
+    if (t_orb == orb) throw std::runtime_error("orbital already occupied");
+    if (t_orb < orb) break;
+    *(t_orbs->Mutable(i)) = t_orbs->Get(i - 1);
+  }
+  *(t_orbs->Mutable(i)) = orb;
+}
+
+void SpinDetUtil::remove_orbital(
+    google::protobuf::RepeatedField<google::protobuf::int32>* t_orbs,
+    const int orb) {
+  bool found = false;
+  const int n_t_orbs = t_orbs->size();
+  for (int i = 0; i < n_t_orbs; i++) {
+    const int t_orb = t_orbs->Get(i);
+    if (found) *(t_orbs->Mutable(i - 1)) = t_orb;
+    if (t_orb == orb) found = true;
+    if (t_orb > orb && !found) break;
+  }
+  if (found) t_orbs->RemoveLast();
+}
