74eb6c0bb669280242351311f8410f3dd027cf24
Junhao Li <jl2922@cornell.edu>
2017-10-03 22:31:13 -0400

    davidson diagonalization (#9)

--- ci.sh
+++ ci.sh
@@ -4,23 +4,21 @@
 test -n $CC && unset CC
 test -n $CXX && unset CXX
 
-set -x
-
-# Install OpenMPI.
+# Install or Load OpenMPI.
 if [ -f "$TOOLS_DIR/openmpi/bin/mpic++" ] && [ -f "$TOOLS_DIR/openmpi/bin/mpic++" ]; then
 	echo "Found cached OpenMPI"
 else
 	echo "Downloading OpenMPI Source"
   mkdir -p downloads
   cd downloads
-	wget -O openmpi-3.0.0.tar.bz2 https://www.open-mpi.org/software/ompi/v3.0/downloads/openmpi-3.0.0.tar.bz2 &> wget.log
+	wget -O openmpi-3.0.0.tar.bz2 https://www.open-mpi.org/software/ompi/v3.0/downloads/openmpi-3.0.0.tar.bz2
 	tar xjf openmpi-3.0.0.tar.bz2
 	echo "Configuring and building OpenMPI"
 	cd openmpi-3.0.0
   mkdir -p $TOOLS_DIR/openmpi
-	./configure --prefix=$TOOLS_DIR/openmpi CC=$C_COMPILER CXX=$CXX_COMPILER &> configure.log
-	make -j 8 &> make.log
-	make install &> install.log
+	./configure --prefix=$TOOLS_DIR/openmpi CC=$C_COMPILER CXX=$CXX_COMPILER
+	make -j 8
+	make install
 	echo "Completed"
 	echo
 	cd ../../
@@ -28,21 +26,21 @@ fi
 export PATH=$TOOLS_DIR/openmpi/bin:$PATH
 export LD_LIBRARY_PATH=$TOOLS_DIR/openmpi/lib:$LD_LIBRARY_PATH
 
-# Install Protocol Buffers.
+# Install or Load Protocol Buffers.
 if [ -f "$TOOLS_DIR/protobuf/bin/protoc" ]; then
 	echo "Found cached Protocol Buffers"
 else
 	echo "Downloading Protocol Buffers"
   mkdir -p downloads
   cd downloads
-	wget -O protobuf-cpp-3.4.1.tar.gz https://github.com/google/protobuf/releases/download/v3.4.1/protobuf-cpp-3.4.1.tar.gz &> wget.log
+	wget -O protobuf-cpp-3.4.1.tar.gz https://github.com/google/protobuf/releases/download/v3.4.1/protobuf-cpp-3.4.1.tar.gz
 	tar xzf protobuf-cpp-3.4.1.tar.gz
 	echo "Configuring and building Protocol Buffers"
 	cd protobuf-3.4.1
   mkdir -p $TOOLS_DIR/protobuf
-	./configure --prefix=$TOOLS_DIR/protobuf CC=$C_COMPILER CXX=$CXX_COMPILER &> configure.log
-	make -j 8 &> make.log
-	make install &> install.log
+	./configure --prefix=$TOOLS_DIR/protobuf CC=$C_COMPILER CXX=$CXX_COMPILER
+	make -j 8
+	make install
 	echo "Completed"
 	echo
 	cd ../../
@@ -50,7 +48,7 @@ fi
 export PATH=$TOOLS_DIR/protobuf/bin:$PATH
 export LD_LIBRARY_PATH=$TOOLS_DIR/protobuf/lib:$LD_LIBRARY_PATH
 
-# Install Boost (with MPI).
+# Install or Load Boost (with MPI and serialization).
 if [ -f "$TOOLS_DIR/boost/lib/libboost_mpi.so" ] || [ -f "$TOOLS_DIR/boost/lib/libboost_mpi.dylib" ]; then
 	echo "Found cached Boost"
 else
@@ -62,11 +60,11 @@ else
 	echo "Configuring and building Boost"
 	cd boost_1_65_1
   mkdir -p $TOOLS_DIR/boost
-  ./bootstrap.sh &> bootstrap.log
+  ./bootstrap.sh
   echo 'libraries =  --with-mpi --with-serialization ;' >> project-config.jam
   echo 'using mpi : mpic++ ;' >> project-config.jam
 	echo 'using gcc : 6 ;' >> project-config.jam
-	./b2 -j8 --prefix=$TOOLS_DIR/boost install &> install.log
+	./b2 -j8 --prefix=$TOOLS_DIR/boost install
 	echo "Completed"
 	echo
 	cd ../../
@@ -74,16 +72,14 @@ fi
 export PATH=$TOOLS_DIR/boost/bin:$PATH
 export LD_LIBRARY_PATH=$TOOLS_DIR/boost/lib:$LD_LIBRARY_PATH
 
-# Install Lockless.
+# Install or Load Lockless.
 if [ -f "$TOOLS_DIR/lockless/lib/libllalloc.so" ]; then
 	echo "Found cached Lockless"
-elif [ "$(uname)" == "Darwin" ]; then
-  echo "Lockless skipped on MacOS"
 else
 	echo "Downloading Lockless"
   mkdir -p downloads
   cd downloads
-	wget -O lockless-1.3.tar.gz https://github.com/jl2922/lockless_allocator/archive/lockless-1.3.tar.gz &> wget.log
+	wget -O lockless-1.3.tar.gz https://github.com/jl2922/lockless_allocator/archive/lockless-1.3.tar.gz
 	tar xzf lockless-1.3.tar.gz
 	echo "Configuring and building Lockless"
 	cd lockless_allocator-lockless-1.3
@@ -98,18 +94,24 @@ else
 fi
 export LD_LIBRARY_PATH=$TOOLS_DIR/lockless/lib:$LD_LIBRARY_PATH
 
-# Install Google Test.
-if [ -f "gtest/googletest/include/gtest/gtest.h" ]; then
-	echo "Found Google Test"
-else
-	echo "Downloading Google Test"
-	wget -O release-1.8.0.tar.gz https://github.com/google/googletest/archive/release-1.8.0.tar.gz &> wget.log
-	tar xzf release-1.8.0.tar.gz
-	rm release-1.8.0.tar.gz
-	mv googletest-release-1.8.0 gtest
-	echo "Completed"
-	echo
-fi
+# Download Eigen.
+echo "Downloading Eigen"
+wget -O eigen-3.3.4.tar.bz2 http://bitbucket.org/eigen/eigen/get/3.3.4.tar.bz2
+tar xjf eigen-3.3.4.tar.bz2
+rm eigen-3.3.4.tar.bz2
+mkdir -p $TOOLS_DIR/eigen/include
+mv eigen-eigen-5a0156e40feb/Eigen $TOOLS_DIR/eigen/include/
+echo "Completed"
+echo
+
+# Download Google Test.
+echo "Downloading Google Test"
+wget -O release-1.8.0.tar.gz https://github.com/google/googletest/archive/release-1.8.0.tar.gz
+tar xzf release-1.8.0.tar.gz
+rm release-1.8.0.tar.gz
+mv googletest-release-1.8.0 gtest
+echo "Completed"
+echo
 
 cp ci.mk local.mk
 make proto
