d269ef5b5560ec22f0b639eeaa70a58897c67885
Junhao Li <jl2922@cornell.edu>
2017-12-11 13:19:24 -0500

    .

--- src/solver/solver.cc
+++ src/solver/solver.cc
@@ -22,7 +22,7 @@
 #define TABLE_FORMAT_LL "%'17llu"
 #define MAX_HASH_LOAD 1.618
 #define PT_RESULTS_LOG "pt_results.csv"
-#define MAX_DETS_PER_FILE 30000
+#define MAX_DETS_PER_FILE 20000000
 
 class UncertainResult {
  public:
