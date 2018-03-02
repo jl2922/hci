269ee4cc3a2d26dd4f07baa1c62b3f81a3c514ff
Junhao Li <streaver91@gmail.com>
2018-03-02 07:06:43 -0500

    .

--- src/solver/connections.cc
+++ src/solver/connections.cc
@@ -404,7 +404,7 @@ void ConnectionsImpl::update_hamiltonian(const int det_id) {
     if (std::abs(H) < std::numeric_limits<double>::epsilon()) continue;
     if (det_id >= 158954 && alpha_det_id < 158954 && std::abs(abstract_system->coefs[alpha_det_id] * H) < 0.0001) continue;
     if (det_id < 158954 && alpha_det_id >= 158954 && std::abs(coef * H) < 0.0001) continue;
-    //if (det_id >= 158954 && alpha_det_id >= 158954) continue;
+    if (det_id >= 158954 && alpha_det_id >= 158954) continue;
     if (det_id >= 158954 && alpha_det_id >= 158954 && std::abs(coef * H) < 0.0001 && 
          std::abs(abstract_system->coefs[alpha_det_id] * H) < 0.0001) continue;
     res.push_back(std::make_pair(alpha_det_id, H));
@@ -423,7 +423,7 @@ void ConnectionsImpl::update_hamiltonian(const int det_id) {
     if (std::abs(H) < std::numeric_limits<double>::epsilon()) continue;
     if (det_id >= 158954 && beta_det_id < 158954 && std::abs(abstract_system->coefs[beta_det_id] * H) < 0.0001) continue;
     if (det_id < 158954 && beta_det_id >= 158954 && std::abs(coef * H) < 0.0001) continue;
-    //if (det_id >= 158954 && beta_det_id >= 158954) continue;
+    if (det_id >= 158954 && beta_det_id >= 158954) continue;
     if (det_id >= 158954 && beta_det_id >= 158954 && std::abs(coef * H) < 0.0001 && 
          std::abs(abstract_system->coefs[beta_det_id] * H) < 0.0001) continue;
     res.push_back(std::make_pair(beta_det_id, H));
@@ -454,7 +454,7 @@ void ConnectionsImpl::update_hamiltonian(const int det_id) {
         if (std::abs(H) < std::numeric_limits<double>::epsilon()) continue;
         if (det_id >= 158954 && related_det_id < 158954 && std::abs(abstract_system->coefs[related_det_id] * H) < 0.0001) continue;
         if (det_id < 158954 && related_det_id >= 158954 && std::abs(coef * H) < 0.0001) continue;
-        //if (det_id >= 158954 && related_det_id >= 158954) continue;
+        if (det_id >= 158954 && related_det_id >= 158954) continue;
         if (det_id >= 158954 && related_det_id >= 158954 && std::abs(coef * H) < 0.0001 && 
              std::abs(abstract_system->coefs[related_det_id] * H) < 0.0001) continue;
         res.push_back(std::make_pair(related_det_id, H));
